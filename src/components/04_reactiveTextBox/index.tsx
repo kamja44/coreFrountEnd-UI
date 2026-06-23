7fffcbb9e69a4b155b88d72345c786994b7ce468
kamja44 <88872409+kamja44@users.noreply.github.com>
2026-06-23 11:22:15 +0900

    4-2 textArea(Replica)

--- src/components/04_reactiveTextBox/index.tsx
+++ src/components/04_reactiveTextBox/index.tsx
@@ -1,11 +1,13 @@
 import cx from "./cx";
 import ReactiveTextBoxes1_React from "./ReactiveTextBoxes1_React";
+import ReactiveTextBoxes2_React from "./ReactiveTextBoxes2_React";
 
 const ReactiveTextBoxes = () => {
   return (
     <div className={cx("ReactiveTextBoxes")}>
       <h2>반응형 텍스트박스</h2>
       <ReactiveTextBoxes1_React />
+      <ReactiveTextBoxes2_React />
     </div>
   );
 };
